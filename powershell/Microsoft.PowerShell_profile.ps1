fea664f3faae4201f7139ecd9e0c467051c42285
parsa <133803919+r4ype@users.noreply.github.com>
2024-07-10 18:32:29 +0330

    Update Microsoft.PowerShell_profile.ps1
    most used directories added

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,9 +1,21 @@
 Invoke-Expression (&starship init powershell)
+
 function New-File {
     param (
         [string]$path
     )
     New-Item -Path $path -ItemType File
 }
+
+function moveToDoc {
+    cd ~\Documents
+}
+
+function moveToSrc {
+    cd ~\Src
+}
+
 Set-Alias touch New-File
-Set-Alias -Name l -Value lsd
+Set-Alias l lsd
+Set-Alias doc moveToDoc
+Set-Alias src moveToSrc
